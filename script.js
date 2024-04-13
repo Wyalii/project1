bef507e65b3ad1e20f88218852b0fff79230ef6f
Wyalii <lawxg123@gmail.com>
2024-04-13 23:36:50 +0400

    half of first page is done, resposive.

---
+++ script.js
@@ -0,0 +1,32 @@
+let closeMenu = document.getElementById('closeMenu');
+let openMenu = document.getElementById('openMenu');
+let mobile = document.getElementById('mobile');
+
+function openMobileMenu() {
+    mobile.style.display = "flex";
+    openMenu.style.display = "none"; 
+    document.querySelector('.part1-body').style.display = "none" 
+    document.querySelector('.part1-footer').style.display = "none"
+}
+
+function closeMobileMenu() {
+    mobile.style.display = "none";
+    openMenu.style.display = "block";
+    document.querySelector('.part1-body').style.display = "block" 
+    document.querySelector('.part1-footer').style.display = "flex"
+}
+
+
+openMenu.addEventListener('click', openMobileMenu);
+closeMenu.addEventListener('click', closeMobileMenu);
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth <= 768) {
+        openMenu.style.display = "block";
+    } else {
+        openMenu.style.display = "none";
+    }
+});
+
+
+    
