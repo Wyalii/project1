4e93860e1777f0d8bb51004698db9309670d9e29
Wyalii <lawxg123@gmail.com>
2024-04-23 21:17:58 +0400

    added new pages they are resposive

--- flights.js
+++ flights.js
@@ -1,18 +1,18 @@
 let closeMenu = document.getElementById('closeMenu');
-let openMenu = document.getElementById('openMenu');
+let openMenu = document.getElementById('openMenuBox');
 let mobile = document.getElementById('mobile');
 
 function openMobileMenu() {
     mobile.style.display = "flex";
-    openMenu.style.display = "none"; 
+    openMenu.style.display = "none";
+    closeMenu.style.display = "block" 
     
 }
 
 function closeMobileMenu() {
     mobile.style.display = "none";
-    openMenu.style.display = "block";
-    document.querySelector('.part1-body').style.display = "block" 
-    document.querySelector('.part1-footer').style.display = "flex"
+    openMenu.style.display = "flex";
+    
 }
 
 
@@ -21,11 +21,9 @@ closeMenu.addEventListener('click', closeMobileMenu);
 
 window.addEventListener('resize', () => {
     if (window.innerWidth <= 768) {
-        openMenu.style.display = "block";
+        openMenu.style.display = "flex";
     } else {
         openMenu.style.display = "none";
+        
     }
 });
-
-
-    
